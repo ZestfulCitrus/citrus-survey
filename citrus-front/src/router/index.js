09644a3a9c79a9b5414a540c7dd9666d9f0adf75
zestfulcitrus <1243137612@qq.com>
2024-04-13 17:57:37 +0800

    set url hash

--- citrus-front/src/router/index.js
+++ citrus-front/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Survey from '../views/Survey.vue'
 
 const router = createRouter({
+  mode: 'hash', // 设置为哈希模式
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
