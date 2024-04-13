67b127af243e41044f8889a00a35d5a002f70677
zestfulcitrus <1243137612@qq.com>
2024-04-13 18:02:44 +0800

    add verceljson

--- citrus-front/src/router/index.js
+++ citrus-front/src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Survey from '../views/Survey.vue'
 
 const router = createRouter({
-  mode: 'hash', // 设置为哈希模式
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
